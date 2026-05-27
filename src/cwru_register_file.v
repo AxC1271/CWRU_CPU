09f21d4831849092a24bf20be8ba8a2b667b5331
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2026-05-26 20:21:30 -0700

    changed reset behavior of register file

--- src/cwru_register_file.v
+++ src/cwru_register_file.v
@@ -16,19 +16,29 @@ module cwru_register_file # (
 
     reg [WIDTH-1:0] registers [31:0];
 
-    genvar g;
-    generate
-        for (g = 0; g < 32; g = g + 1) begin : rst_regs
-            always @(posedge clk or negedge rst_n) begin
-                if (!rst_n) begin
-                    registers[g] <= 32'b0;
-                end else begin
-                    if (reg_write && (wr_addr == g) && (g != 0))
-                        registers[g] <= wr_data;
-                end
-            end
+    always @(posedge clk or negedge rst_n) begin : reg_block
+        if (!rst_n) begin
+            registers[0]  <= 32'b0; registers[1]  <= 32'b0;
+            registers[2]  <= 32'b0; registers[3]  <= 32'b0;
+            registers[4]  <= 32'b0; registers[5]  <= 32'b0;
+            registers[6]  <= 32'b0; registers[7]  <= 32'b0;
+            registers[8]  <= 32'b0; registers[9]  <= 32'b0;
+            registers[10] <= 32'b0; registers[11] <= 32'b0;
+            registers[12] <= 32'b0; registers[13] <= 32'b0;
+            registers[14] <= 32'b0; registers[15] <= 32'b0;
+            registers[16] <= 32'b0; registers[17] <= 32'b0;
+            registers[18] <= 32'b0; registers[19] <= 32'b0;
+            registers[20] <= 32'b0; registers[21] <= 32'b0;
+            registers[22] <= 32'b0; registers[23] <= 32'b0;
+            registers[24] <= 32'b0; registers[25] <= 32'b0;
+            registers[26] <= 32'b0; registers[27] <= 32'b0;
+            registers[28] <= 32'b0; registers[29] <= 32'b0;
+            registers[30] <= 32'b0; registers[31] <= 32'b0;
+        end else begin
+            if (reg_write && (wr_addr != 5'b0))
+                registers[wr_addr] <= wr_data;
         end
-    endgenerate
+    end
 
     assign rd_data1 = registers[rd_addr1];
     assign rd_data2 = registers[rd_addr2];
