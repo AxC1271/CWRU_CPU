7ecbc65dca85a23c599e6a0a1e28ab2110ee72f1
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2026-05-26 20:14:52 -0700

    fixed register file

--- src/cwru_register_file.v
+++ src/cwru_register_file.v
@@ -1,48 +1,36 @@
-`timescale 1ns / 1ps 
-
-// quick note, based on how you write
-// the register file, should rd_data/wr_data
-// be reg or wire? this is an open-ended question
 module cwru_register_file # (
     parameter WIDTH = 32
 )(
     input wire clk,
     input wire rst_n,
 
-    // rd interface
-    input wire[4:0] rd_addr1,
-    input wire[4:0] rd_addr2,
-    output wire[WIDTH-1:0] rd_data1,
-    output wire[WIDTH-1:0] rd_data2,
+    input wire [4:0]        rd_addr1,
+    input wire [4:0]        rd_addr2,
+    output wire [WIDTH-1:0] rd_data1,
+    output wire [WIDTH-1:0] rd_data2,
 
-    // wr interface
-    input wire[4:0] wr_addr,
-    input wire reg_write,
-    input wire[WIDTH-1:0] wr_data
+    input wire [4:0]        wr_addr,
+    input wire              reg_write,
+    input wire [WIDTH-1:0]  wr_data
 );
 
-    // define your logic starting here
-    // quick pointers: register 0 shouldn't be writeable
-    // decide if your reads use combinational or sequential logic
-    reg [WIDTH-1:0] registers [WIDTH-1:0];
+    reg [WIDTH-1:0] registers [31:0];
 
-
-    integer i;
-
-    always @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            for (i = 0; i < 32; i = i + 1)
-                registers[i] <= 32'b00;
-            end
-        else begin
-            if (reg_write) begin
-                if (wr_addr != 5'b00000) begin
-                    registers[wr_addr] <= wr_data;
+    genvar g;
+    generate
+        for (g = 0; g < 32; g = g + 1) begin : rst_regs
+            always @(posedge clk or negedge rst_n) begin
+                if (!rst_n) begin
+                    registers[g] <= 32'b0;
+                end else begin
+                    if (reg_write && (wr_addr == g) && (g != 0))
+                        registers[g] <= wr_data;
                 end
             end
-        end      
-    end
+        end
+    endgenerate
 
     assign rd_data1 = registers[rd_addr1];
     assign rd_data2 = registers[rd_addr2];
+
 endmodule
