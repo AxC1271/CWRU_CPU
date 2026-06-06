194b2d66f43024b0704da86e5b04081841bd7dbe
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2026-06-06 14:06:50 -0700

    istg yosys behave

--- src/cwru_register_file.v
+++ src/cwru_register_file.v
@@ -16,16 +16,30 @@ module cwru_register_file #(
 
     always @(posedge clk) begin
         if (!rst_n) begin
-            regs[1]  <= 0; regs[2]  <= 0; regs[3]  <= 0; regs[4]  <= 0;
-            regs[5]  <= 0; regs[6]  <= 0; regs[7]  <= 0; regs[8]  <= 0;
-            regs[9]  <= 0; regs[10] <= 0; regs[11] <= 0; regs[12] <= 0;
-            regs[13] <= 0; regs[14] <= 0; regs[15] <= 0; regs[16] <= 0;
-            regs[17] <= 0; regs[18] <= 0; regs[19] <= 0; regs[20] <= 0;
-            regs[21] <= 0; regs[22] <= 0; regs[23] <= 0; regs[24] <= 0;
-            regs[25] <= 0; regs[26] <= 0; regs[27] <= 0; regs[28] <= 0;
-            regs[29] <= 0; regs[30] <= 0; regs[31] <= 0;
+            r1<=0;  r2<=0;  r3<=0;  r4<=0;  r5<=0;  r6<=0;  r7<=0;  r8<=0;
+            r9<=0;  r10<=0; r11<=0; r12<=0; r13<=0; r14<=0; r15<=0; r16<=0;
+            r17<=0; r18<=0; r19<=0; r20<=0; r21<=0; r22<=0; r23<=0; r24<=0;
+            r25<=0; r26<=0; r27<=0; r28<=0; r29<=0; r30<=0; r31<=0;
         end else if (we) begin
-            regs[wr_addr] <= wr_data;
+            case (wr_addr)
+                5'd1:  r1<=wr_data;   5'd2:  r2<=wr_data;
+                5'd3:  r3<=wr_data;   5'd4:  r4<=wr_data;
+                5'd5:  r5<=wr_data;   5'd6:  r6<=wr_data;
+                5'd7:  r7<=wr_data;   5'd8:  r8<=wr_data;
+                5'd9:  r9<=wr_data;   5'd10: r10<=wr_data;
+                5'd11: r11<=wr_data;  5'd12: r12<=wr_data;
+                5'd13: r13<=wr_data;  5'd14: r14<=wr_data;
+                5'd15: r15<=wr_data;  5'd16: r16<=wr_data;
+                5'd17: r17<=wr_data;  5'd18: r18<=wr_data;
+                5'd19: r19<=wr_data;  5'd20: r20<=wr_data;
+                5'd21: r21<=wr_data;  5'd22: r22<=wr_data;
+                5'd23: r23<=wr_data;  5'd24: r24<=wr_data;
+                5'd25: r25<=wr_data;  5'd26: r26<=wr_data;
+                5'd27: r27<=wr_data;  5'd28: r28<=wr_data;
+                5'd29: r29<=wr_data;  5'd30: r30<=wr_data;
+                5'd31: r31<=wr_data;
+                default: ;
+            endcase
         end
     end
 
