fd0b88ccbdc839526909455d0e8228ad64944bd9
Andrew Chen <109437047+AxC1271@users.noreply.github.com>
2026-06-06 14:01:53 -0700

    fixed register_file for yosys gds conversion

--- src/cwru_register_file.v
+++ src/cwru_register_file.v
@@ -14,11 +14,16 @@ module cwru_register_file #(
     reg [WIDTH-1:0] regs [1:31];
     wire we = reg_write && (wr_addr != 5'b0);
 
-    integer i;
     always @(posedge clk) begin
         if (!rst_n) begin
-            for (i = 1; i < 32; i = i + 1)
-                regs[i] <= {WIDTH{1'b0}};
+            regs[1]  <= 0; regs[2]  <= 0; regs[3]  <= 0; regs[4]  <= 0;
+            regs[5]  <= 0; regs[6]  <= 0; regs[7]  <= 0; regs[8]  <= 0;
+            regs[9]  <= 0; regs[10] <= 0; regs[11] <= 0; regs[12] <= 0;
+            regs[13] <= 0; regs[14] <= 0; regs[15] <= 0; regs[16] <= 0;
+            regs[17] <= 0; regs[18] <= 0; regs[19] <= 0; regs[20] <= 0;
+            regs[21] <= 0; regs[22] <= 0; regs[23] <= 0; regs[24] <= 0;
+            regs[25] <= 0; regs[26] <= 0; regs[27] <= 0; regs[28] <= 0;
+            regs[29] <= 0; regs[30] <= 0; regs[31] <= 0;
         end else if (we) begin
             regs[wr_addr] <= wr_data;
         end
